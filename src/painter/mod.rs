fb837bdc3183f32dccd4711f8636aa35540cf466
Alexandre Lehmann Holzhey <saleiro@gmail.com>
2024-01-07 18:45:15 +0100

    add structs

--- src/painter/mod.rs
+++ src/painter/mod.rs
@@ -4,9 +4,30 @@ pub struct Screen {
     buffer: Vec<u32>,
 }
 
-pub struct Point {
+pub struct Position {
     x: usize,
     y: usize,
+}
+
+pub trait Color {
+    fn get_color_value(self) -> u32;
+}
+
+pub struct ColorRGB {
+    r: u8,
+    g: u8,
+    b: u8,
+}
+
+impl Color for ColorRGB {
+    fn get_color_value(self) -> u32 {
+        self.b as u32
+    }
+}
+
+pub struct Pixel {
+    position: Position,
+    color: dyn Color,
 }
 
 impl Screen {
@@ -18,9 +39,9 @@ impl Screen {
         }
     }
 
-    pub fn plot(&mut self, point: Point, color: u32) {
-        let pos = (point.y * self.width) + point.x;
-        self.buffer[pos] = color
+    pub fn plot(&mut self, pixel: &Pixel) {
+        let pos = (pixel.position.y * self.width) + pixel.position.x;
+        self.buffer[pos] = pixel.color.get_color_value()
     }
 
     pub fn get_buffer(&self) -> &Vec<u32> {
@@ -28,8 +49,20 @@ impl Screen {
     }
 }
 
-impl Point {
+impl Position {
     pub fn new(x: usize, y: usize) -> Self {
         Self { x, y }
+    }
+}
+
+impl Pixel {
+    pub fn new(position: Position, color: impl Color) -> Self {
+        Self { position, color }
+    }
+}
+
+impl ColorRGB {
+    pub fn new(r: u8, g: u8, b: u8) -> Self {
+        Self { r, g, b }
     }
 }
